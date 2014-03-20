ec6c4ca76b57fe14fa4815f0b796eb028e5f155d
Trung Ngo <ndtrung4419@gmail.com>
2014-03-20 15:49:51 +0700

    Add Vietnamese keyboard layout and wordlist

---
+++ apps/keyboard/js/layouts/vi.js
@@ -0,0 +1,37 @@
+Keyboards.vi = {
+  label: 'Vietnamese',
+  imEngine: 'latin',
+  types: ['text', 'url', 'email'],
+  autoCorrectLanguage: 'vi',
+  menuLabel: 'Tiếng Việt',
+  alt: {
+    a: 'àáảãạăằắẳẵặâầấẩẫậ',
+    e: 'èéẻẽẹềếểễệê',
+    i: 'ìíỉĩị',
+    o: 'òóỏõọồốổỗộôờớởỡợơ',
+    u: 'ùúủũụừứửữựư',
+    y: 'ỳýỷỹỵ',
+    d: 'đ',
+    '.': ',?!;:'
+  },
+  keys: [
+    [
+      { value: 'q' }, { value: 'w' }, { value: 'e' } , { value: 'r' },
+      { value: 't' } , { value: 'y' }, { value: 'u' } , { value: 'i' },
+      { value: 'o' }, { value: 'p' }
+    ], [
+      { value: 'a' }, { value: 's' }, { value: 'd' }, { value: 'f' },
+      { value: 'g' } , { value: 'h' }, { value: 'j' }, { value: 'k' },
+      { value: 'l' },
+      { value: ':', visible: ['url']}, { value: '_', visible: ['email']}
+    ], [
+      { value: '⇪', ratio: 1.5, keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
+      { value: 'z' }, { value: 'x' }, { value: 'c' }, { value: 'v' },
+      { value: 'b' }, { value: 'n' }, { value: 'm' },
+      { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+    ], [
+      { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+      { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+    ]
+  ]
+};
